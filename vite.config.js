23eef972158d774ff1ec8bd45d46e35a29255e95
Saad A Ghani <saadhku2@gmail.com>
2026-05-07 22:10:23 -0400

    repo name updated

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // Change this to match your GitHub repo name, e.g. '/hifdh-app/'
 // If using a custom domain, set to '/'
-const BASE_URL = '/hifdh-app/'
+const BASE_URL = '/Hifdh-Companion/'
 
 export default defineConfig({
   plugins: [react()],
